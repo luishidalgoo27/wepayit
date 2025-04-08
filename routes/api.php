6d9e5bf1b031c94c080512343bc856d23c41f5a0
luishdalgo <lhs0003@alu.medac.es>
2025-04-08 20:24:26 +0200

    Commit Group Controller

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\GroupController;
 
 Route::post('/register', [AuthController::class, 'createUser']);
 Route::post('/login', [AuthController::class, 'loginUser']);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-
+    
+    Route::post('/group', [GroupController::class, 'create']);
     
 
 
