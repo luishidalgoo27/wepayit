b272c222cda8dbfe5b546fe35bb61d8c287b1e43
luishdalgo <lhs0003@alu.medac.es>
2025-04-08 20:24:26 +0200

    Commit Group Controller

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GroupRequest;
+use App\Services\GroupService;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function __construct(
+        private GroupService $groupService,
+        ) {
+        $this->groupService = $groupService; 
+    }
+
+    public function create(GroupRequest $req)
+    {   
+        $group = $this->groupService->create($req);
+        return response()->json($group, 201);
+    }
+}
