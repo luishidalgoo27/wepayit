541dd6c226d28ad19ca280352dad329dfa473a8b
luishdalgo <lhs0003@alu.medac.es>
2025-04-08 18:03:19 +0200

    Project Structure

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function create(){
+        /* Group::create([
+            
+        ]); */
+    }
+
+
+
+
+
+}
