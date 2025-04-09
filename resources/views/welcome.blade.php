2612bec75de07d908e248346b3f794a8bb11c021
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-04-09 20:54:33 +0200

    estructuracion del frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite + React</title>
     @viteReactRefresh
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/main.jsx'])
   </head>
   <body>
     <div id="root"></div>
