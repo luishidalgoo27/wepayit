a9643a7a13896a1d624408e81161352636c64a8b
luishdalgo <lhs0003@alu.medac.es>
2025-04-09 12:13:20 +0200

    Invite Group Function

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\GroupInvitationRequest;
 use App\Http\Requests\GroupRequest;
 use App\Services\GroupService;
 use Illuminate\Http\Request;
@@ -24,5 +25,11 @@ class GroupController extends Controller
     {   
         $groups = $this->groupService->get();
         return response()->json($groups, 201);
+    }
+
+    public function addUser(GroupInvitationRequest $req)
+    {
+        $user = $this->groupService->addUser($req);
+        return response()->json($user, 201);
     }
 }
---
+++ app/Http/Requests/GroupInvitationRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GroupInvitationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'group_id' => 'integer|exists:groups,id',
+            'guest_email' => 'required|email|exists:users,email'
+        ];
+    }
+
+    public function messages(): array
+{
+    return [
+        'group_id.exists' => 'El grupo seleccionado no existe.',
+        'guest_user_id.exists' => 'El usuario que estás intentando invitar no existe.',
+    ];
+}
+}
