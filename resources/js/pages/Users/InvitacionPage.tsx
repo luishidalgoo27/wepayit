15228be4bd32ffac3ec91f1e602bf1ece53154be
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-04-22 20:42:31 +0200

    nuevas paginas modificadas

--- resources/js/pages/Users/InvitacionPage.tsx
+++ resources/js/pages/Users/InvitacionPage.tsx
@@ -1,9 +1,9 @@
-export default function InvitacionPage(){
-    return(
+export const InvitacionPage = () => {
+    return (
         <main className="container mx-auto p-4">
             <div className="text-center">
                 <h1>PULSA ESTE BOTON PARA UNIRTE AL GRUPO DE .........</h1>
-                <button type="button " className="bg-slate-500 rounded-md w-24 h-8">Unete</button>
+                <button className="bg-slate-500 rounded-md w-24 h-8">Unete</button>
             </div>
         </main>
     )
