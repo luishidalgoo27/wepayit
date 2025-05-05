cad8c2880210ecd7952b586ff0736622802183fb
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-05-05 19:54:04 +0200

    fondo y colores

--- resources/js/pages/Users/LoginPage.tsx
+++ resources/js/pages/Users/LoginPage.tsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { login } from "@/services/auth";
 import { useNavigate } from "react-router-dom";
 import useGetUser from "@/hooks/useGetUser";
+import toast from "react-hot-toast";
 
 export const LoginPage = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState<string | null>(null)
-  
-  const navigate = useNavigate()
-  const { mutate } = useGetUser()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+  const { mutate } = useGetUser();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -17,10 +18,13 @@ export const LoginPage = () => {
 
     try {
       await login(email, password);
-      await mutate()  
+      await mutate(); // Actualiza el estado del usuario
+      toast.success("Has iniciado sesión correctamente");
       navigate("/");
     } catch (err: any) {
-      setError(err.message || "Error desconocido");
+      const message = err.message || "Error desconocido";
+      setError(message);
+      toast.error(message);
     }
   };
 
@@ -33,7 +37,7 @@ export const LoginPage = () => {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form onSubmit={handleSubmit} className="space-y-6">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-white">
               Email
@@ -45,6 +49,8 @@ export const LoginPage = () => {
                 id="email"
                 autoComplete="email"
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="block w-full rounded-md bg-white px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-[#8FE3C2]"
               />
             </div>
@@ -66,10 +72,18 @@ export const LoginPage = () => {
                 id="password"
                 autoComplete="current-password"
                 required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="block w-full rounded-md bg-white px-3 py-2 text-gray-900 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-[#8FE3C2]"
               />
             </div>
           </div>
+
+          {error && (
+            <p className="text-sm text-red-400 text-center">
+              {error}
+            </p>
+          )}
 
           <div>
             <button
