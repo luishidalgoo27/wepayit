46ef5281e19b679d8e57061ce93c149d48563da0
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-05-07 14:01:01 +0200

    interfaces

--- resources/js/types/group.d.ts
+++ resources/js/types/group.d.ts
@@ -1,6 +1,22 @@
 export interface Group {
-    id: string,
+    id: number,
     name: string,
     description: string|null,
     photo: string|null
+    owner_id: number
+}
+
+export interface UserGroup {
+    id: number,
+    group_id: number,
+    user_id: number,
+}
+
+export interface GroupInvitation {
+    id: number,
+    group_id: number,
+    user_id: number|null,
+    guest_email: string,
+    invitation_code: string,
+    status: boolean,
 }
