3696ad9708e5f66e914ed74864d9d35643c31c4b
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-05-12 20:04:25 +0200

    fotos

--- resources/js/services/groups.ts
+++ resources/js/services/groups.ts
@@ -14,11 +14,12 @@ export const getGroup = async (id:string):Promise<Group> => {
     return res.data
 }
 
-export const createGroup = async (name: string, currency_type: string, description: string ):Promise<Group[]> => {
+export const createGroup = async (name: string, currency_type: string, description: string, image: File|null):Promise<Group> => {
     const res = await api.post(`${API_URL}/group`, {
         name: name,
         currency_type: currency_type,
-        description: description
+        description: description,
+        image: image
     })
     return res.data
 }
