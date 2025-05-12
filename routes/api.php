6412934676f701361b14e23b7004ddf2ada25289
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-05-12 18:06:39 +0200

    rutes api.php fixed

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::delete('/group', [GroupController::class, 'delete']);
     Route::post('/deletePhoto', [GroupController::class, 'deleteImage']);
     
-    Route::get('/getUsers', [GroupController::class, 'getUsers']);
+    Route::post('/getUsers', [GroupController::class, 'getUsers']);
     
     Route::delete('/deleteUser', [UserGroupController::class, 'deleteUser']);
     
     Route::post('/invitation', [UserGroupController::class, 'sendInvitation']);
     Route::get('/invitations/accept/{code}', [UserGroupController::class, 'acceptInvitation']);
     
-    Route::get('/expense', [ExpensesController::class, 'getExpenses']);
+    Route::post('/expenses', [ExpensesController::class, 'getExpenses']);
     Route::patch('/expense', [ExpensesController::class, 'update']);
     Route::post('/expense', [ExpensesController::class, 'create']);
     Route::delete('/expense', [ExpensesController::class, 'delete']);
