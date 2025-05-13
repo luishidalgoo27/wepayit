9f9062ec41de386c7932fc21369001bb88af4ca7
pedroangelbl <pedroangelbeltranlopez@gmail.com>
2025-05-13 17:30:04 +0200

    edit profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.tsx'])
   </head>
-  <body class="bg-gradient-to-b from-200 to-400 dark:bg-gradient-to-b dark:from-950 dark:to-800 dark:text-50 text-950 -mt-12">
+  <body class="bg-gradient-to-b from-200 to-400 dark:bg-gradient-to-b dark:from-950 dark:to-800 dark:text-50 text-950">
     <div id="root"></div>
   </body>
 </html>
