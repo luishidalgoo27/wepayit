369a0a6a6d6839fc11aebc3044f58a7c7b077e68
luishdalgo <lhs0003@alu.medac.es>
2025-05-14 18:25:34 +0200

    Payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -17,9 +17,12 @@ class PaymentService
         $groupId = $req->group_id;
 
         $expenseIds = Expense::where('group_id', $groupId)->pluck('id');
+        $paymentGroup = Payment::where('payer_id', Auth::id())->whereIn('expense_id', $expenseIds)->sum('amount');
         $paymentUsers = Payment_user::where('user_id', Auth::id())->whereIn('expense_id', $expenseIds)->sum('amount');    
 
-        return $paymentUsers;
+        $paymentTotal = $paymentGroup + $paymentUsers;
+
+        return $paymentTotal;
     }
 
     public function getPaymentGroup(PaymentRequest $req)
