b96553f739dc3f99faa173966c683c61b56e0ee1
luishdalgo <lhs0003@alu.medac.es>
2025-05-14 20:54:35 +0200

    CommitProduction

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::post('/register', [AuthController::class, 'createUser'])->name('auth.register');
 Route::post('/login', [AuthController::class, 'loginUser'])->name('auth.login');
-Route::get('/verify-email', [AuthController::class, 'verifyEmail'])->name('verification.verify');
+Route::get('/verify-email', [AuthController::class, 'verifyEmail'])->name('verification.verify')->middleware(['signed']);;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', [UserController::class, 'getUser']);
