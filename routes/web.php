d0ff79f10a18e471eec5badaf4ee7f316d92cd2b
luishdalgo <lhs0003@alu.medac.es>
2025-05-15 20:12:27 +0200

    Vista Invitacion

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserGroupController;
+
 
 // Ruta de fallback para React
 Route::get('/{any}', function () {
     return view('welcome'); 
 })->where('any', '.*');
+
 
 /* Route::get('/prueba', function () {
     return view('prueba'); 
