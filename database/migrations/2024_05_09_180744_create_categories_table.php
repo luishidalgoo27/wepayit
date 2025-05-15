5ff14bcb94058441da26f426bcd7af4ac6a62d95
luishdalgo <lhs0003@alu.medac.es>
2025-05-15 12:23:37 +0200

    Categories vuelve a ser categories

--- database/migrations/2024_05_09_180744_create_categories_table.php
+++ database/migrations/2024_05_09_180744_create_categories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('type');
-            $table->string('emoji')->nullable(); // Nuevo campo para el emoticono
         });
     }
 
