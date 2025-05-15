62a4f1bbce33491622c562e91065bbe4d2f1bf3a
luishdalgo <lhs0003@alu.medac.es>
2025-05-15 18:48:00 +0200

    ?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>WePayIt</title>
+    <title>WePayIt</title> 
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.tsx'])
