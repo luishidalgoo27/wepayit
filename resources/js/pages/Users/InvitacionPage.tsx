6a1e638250717795a6799e9afefafe99d2cc3a56
Rafael López Gómez <rlg0019@alu.medac.es>
2025-05-15 20:49:12 +0200

    correccion vista invitacion

--- resources/js/pages/Users/InvitacionPage.tsx
+++ resources/js/pages/Users/InvitacionPage.tsx
@@ -21,16 +21,23 @@ export const InvitacionPage = () => {
 
 
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
-            <h1 className="text-2xl font-extrabold text-600 mb-10 text-center">
-                ¡Te han invitado a un grupo!
-            </h1>
-            <button
-                onClick={handleAcceptInvitation}
-                className="clickButton font-semibold text-lg px-10 py-4 rounded-full shadow-lg transition"
-            >
-                Unirme al grupo
-            </button>
+        <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br px-4">
+            <div className="bg-50 rounded-3xl shadow-2xl p-8 md:p-16 flex flex-col items-center max-w-2xl w-full">
+                <div className="mb-6 flex flex-col items-center">
+                    <h1 className="text-3xl font-extrabold text-600 text-center mb-5">
+                        ¡Te han invitado a un grupo!
+                    </h1>
+                    <p className="text-500 text-lg text-center">
+                        Únete y empieza a compartir gastos fácilmente con tus amigos.
+                    </p>
+                </div>
+                <button
+                    onClick={handleAcceptInvitation}
+                    className="clickButton font-bold text-lg px-10 py-4 rounded-full shadow-lg w-full text-center mb-4"
+                >
+                    Unirme al grupo
+                </button>
+            </div>
         </div>
     );
 };
