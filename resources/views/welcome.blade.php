d22af164be71c7522398f0ba5f20d58192415ef8
luishdalgo <lhs0003@alu.medac.es>
2025-05-15 18:28:35 +0200

    Commit icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>WePayIt</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.tsx'])
   </head>
