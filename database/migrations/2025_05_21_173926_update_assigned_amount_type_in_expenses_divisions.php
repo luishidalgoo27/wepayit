2e847ee35e19a1d99446d6f82a6ef630c94f5a1d
luishdalgo <lhs0003@alu.medac.es>
2025-05-21 20:04:34 +0200

    Prueba

--- database/migrations/2025_05_21_173926_update_assigned_amount_type_in_expenses_divisions.php
+++ database/migrations/2025_05_21_173926_update_assigned_amount_type_in_expenses_divisions.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('expenses_divisions', function (Blueprint $table) {
-            //
+            $table->decimal('assigned_amount', 10, 2)->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('expenses_divisions', function (Blueprint $table) {
-            //
+            $table->float('assigned_amount')->change();
         });
     }
 };
